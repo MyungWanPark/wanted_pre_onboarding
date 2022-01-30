20443126d5ca7a59593a56c8a77e3e0c7faff800
MyungWanPark <mywanpark@gmail.com>
2022-01-30 17:02:54 +0900

    implement autoComplete

--- custom-component/src/component/autoComplete.jsx
+++ custom-component/src/component/autoComplete.jsx
@@ -1,9 +1,81 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const InputContainer = styled.div``;
+const TotalContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  position: absolute;
+  top: 40%;
+  flex-direction: column;
+  align-items: center;
+`;
 
-const DropBox = styled.ul``;
+const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 10%;
+
+  > .input {
+    padding: 10px;
+    border: none;
+    outline: none;
+    width: 90%;
+    height: 100%;
+    font-size: 16px;
+    background-color: transparent;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    &:focus {
+      box-shadow: 0px 8px 6px -3px rgba(0, 0, 0, 0.3);
+    }
+  }
+  > .input.isInputted {
+    box-shadow: none;
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+  }
+
+  > .input.isInputted.unProperInput {
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+    box-shadow: 0px 8px 6px -3px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const DeleteBtn = styled.button`
+  position: absolute;
+  right: 5%;
+  top: 10%;
+  height: 70%;
+  border: none;
+  background: transparent;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
+const DropBox = styled.ul`
+  width: 90%;
+  list-style: none;
+  border: 1px solid lightgray;
+  border-top: none;
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  margin: 0px;
+  padding: 5px 0px;
+  box-shadow: 0px 8px 6px -3px rgba(0, 0, 0, 0.3);
+  > li {
+    padding-left: 10px;
+  }
+  > li:hover {
+    background-color: #efefef;
+  }
+  > li.selected {
+    background-color: #efefef;
+  }
+`;
 
 const possibleOptions = [
   "antique",
@@ -14,12 +86,14 @@ const possibleOptions = [
 ];
 const Autocomplete = () => {
   const [inputValue, setInputValue] = useState("");
+  const [isInputted, setIsInputted] = useState(false);
   const [options, setOptions] = useState(possibleOptions);
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   useEffect(() => {
     if (!inputValue) {
       setOptions([]);
+      setIsInputted(false);
     }
 
     if (inputValue) {
@@ -32,10 +106,12 @@ const Autocomplete = () => {
   }, [inputValue]);
 
   const handleInputChange = (event) => {
+    setIsInputted(true);
     setInputValue(event.target.value);
   };
 
   const selectDropDown = (clickedOption) => {
+    setIsInputted(true);
     setInputValue(clickedOption);
   };
 
@@ -54,27 +130,40 @@ const Autocomplete = () => {
   };
 
   return (
-    <div>
+    <TotalContainer>
       <InputContainer>
         <input
           type="text"
           value={inputValue}
           onChange={handleInputChange}
           onKeyUp={handleKeyUp}
-        ></input>
-        <div onClick={() => setInputValue("")}>&times;</div>
+          placeholder="type any words"
+          className={
+            isInputted
+              ? options.length !== 0
+                ? `input isInputted properInput`
+                : `input isInputted unProperInput`
+              : "input"
+          }
+        />
+        <DeleteBtn onClick={() => setInputValue("")}>&times;</DeleteBtn>
       </InputContainer>
-
-      <DropBox>
-        {options.map((option, index) => {
-          return (
-            <li key={index} onClick={() => selectDropDown(option)}>
-              {option}
-            </li>
-          );
-        })}
-      </DropBox>
-    </div>
+      {options.length !== 0 ? (
+        <DropBox>
+          {options.map((option, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => selectDropDown(option)}
+                className={selectedIndex === index ? "selected" : ""}
+              >
+                {option}
+              </li>
+            );
+          })}
+        </DropBox>
+      ) : null}
+    </TotalContainer>
   );
 };
 
